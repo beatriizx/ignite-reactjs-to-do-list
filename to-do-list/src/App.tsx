1c775d5524d75f98b6447b3fddacc699e5c71270
Beatriz <beatriz.lira@skeelo.app>
2023-02-16 15:29:29 -0300

    feat: add logo and update styles

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,8 +1,10 @@
 import { CreateTaskInput } from './components/CreateTaskInput';
+import logo from './assets/logo.svg';
 import styles from './styles/app.styles.module.scss';
 export const App = () => {
   return (
     <div className={styles.container}>
+      <img src={logo} alt="" />
       <CreateTaskInput />
     </div>
   );
