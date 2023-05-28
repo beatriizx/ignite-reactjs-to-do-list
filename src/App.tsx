f91fc527873dadc44a0dec3ce598eb385b4176c0
Beatriz <beatriz.lira@skeelo.app>
2023-05-28 14:53:26 -0300

    feat: save tasks in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,76 @@
+/** @format */
+
 import { CreateTaskInput } from './components/CreateTaskInput';
 import logo from './assets/logo.svg';
 import styles from './styles/app.styles.module.scss';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { TasksList } from './components/TasksList';
 interface Task {
-  id: number;
-  task: string;
-  completed: boolean;
+	id: number;
+	task: string;
+	completed: boolean;
 }
 
 export const App = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTask, setNewTask] = useState('');
+	const [tasks, setTasks] = useState<Task[]>([]);
+	const [newTask, setNewTask] = useState('');
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    setTasks([...tasks, { id: Math.random(), task: newTask, completed: false }]);
-  };
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		const newArray = [...tasks, { id: Math.random(), task: newTask, completed: false }];
+		setTasks(newArray);
+		updateLocalStorage(newArray);
+	};
 
-  const handleDeleteTask = (id: number) => {
-    const newArray = tasks.filter((item) => {
-      return item.id != id;
-    });
+	const handleDeleteTask = (id: number) => {
+		const newArray = tasks.filter((item) => {
+			return item.id != id;
+		});
 
-    setTasks(newArray);
-  };
+		setTasks(newArray);
+		updateLocalStorage(newArray);
+	};
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setNewTask(event.target.value);
-  };
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setNewTask(event.target.value);
+	};
 
-  const handleTaskStatusChange = (currentID: number) => {
-    const newTasks = tasks.map((item) => {
-      if (item.id === currentID) item.completed = !item.completed;
-      return item;
-    });
+	const handleTaskStatusChange = (currentID: number) => {
+		const newTasks = tasks.map((item) => {
+			if (item.id === currentID) item.completed = !item.completed;
+			return item;
+		});
 
-    setTasks(newTasks);
-  };
+		setTasks(newTasks);
+		updateLocalStorage(newTasks);
+	};
 
-  return (
-    <div className={styles.container}>
-      <img src={logo} alt="" />
-      <CreateTaskInput onHandleSubmit={handleSubmit} onHandleChange={handleChange} />
-      <TasksList
-        tasks={tasks}
-        handleTaskStatusChange={handleTaskStatusChange}
-        handleDeleteTask={handleDeleteTask}
-      />
-    </div>
-  );
+	const updateLocalStorage = (tasks: Task[]) => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	};
+
+	useEffect(() => {
+		const tasks = localStorage.getItem('tasks');
+		if (tasks) {
+			setTasks(JSON.parse(tasks));
+		}
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<img
+				src={logo}
+				alt=''
+			/>
+			<CreateTaskInput
+				onHandleSubmit={handleSubmit}
+				onHandleChange={handleChange}
+			/>
+			<TasksList
+				tasks={tasks}
+				handleTaskStatusChange={handleTaskStatusChange}
+				handleDeleteTask={handleDeleteTask}
+			/>
+		</div>
+	);
 };
--- src/components/CreateTaskInput.tsx
+++ src/components/CreateTaskInput.tsx
@@ -1,18 +1,28 @@
+/** @format */
+
 import { ChangeEventHandler, FormEventHandler } from 'react';
 import styles from '../styles/CreateTaskInput.module.scss';
 
 interface CreateTaskInputProps {
-  onHandleSubmit: FormEventHandler<HTMLFormElement>;
-  onHandleChange: ChangeEventHandler<HTMLInputElement>;
+	onHandleSubmit: FormEventHandler<HTMLFormElement>;
+	onHandleChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 export const CreateTaskInput = ({ onHandleSubmit, onHandleChange }: CreateTaskInputProps) => {
-  return (
-    <div className={styles.formContainer}>
-      <form onSubmit={onHandleSubmit}>
-        <input type="text" placeholder="Adicione uma tarefa" onChange={onHandleChange} required />
-        <input type="submit" value="Criar"></input>
-      </form>
-    </div>
-  );
+	return (
+		<div className={styles.formContainer}>
+			<form onSubmit={onHandleSubmit}>
+				<input
+					type='text'
+					placeholder='Adicione uma tarefa'
+					onChange={onHandleChange}
+					required
+				/>
+				<input
+					type='submit'
+					value='Criar'
+				/>
+			</form>
+		</div>
+	);
 };
